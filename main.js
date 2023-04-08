ec46f8a39a5359a11a12044890bd3d15a0b9a994
Muradhanov <taghirmuradhanov@yandex.ru>
2023-04-08 21:15:33 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+function toNum(str) {
+    const num = Number(str.replace(/ /g, ""));
+    return num;
+  }
+  
+  function toCurrency(num) {
+    const format = new Intl.NumberFormat("ru-RU", {
+      style: "currency",
+      currency: "RUB",
+      minimumFractionDigits: 0,
+    }).format(num);
+    return format;
+  }
